152de6864b2ab950a60aa4e53d0a6e1a21c9b068
Ivan Chaikouski <ichaikouski@iba.by>
2021-01-17 02:29:10 +0300

    add db connection and entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './controller/app.controller';
+
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppService } from './service/app.service';
+import { AppController } from './controller/app.controller';
+import { StockRequest } from './model/request.model';
+import { Offer } from './model/offer.model';
+import { Role } from './model/role.model';
+import { User } from './model/user.model';
+import { RoleFunction } from './model/function.model';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      entities: [__dirname + '/**/*.model{.ts,.js}'],
+      url: process.env.MONGO_URL,
+      database: process.env.MONGO_DB,
+      type: 'mongodb',
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    }),
+    TypeOrmModule.forFeature([StockRequest, Offer, Role, User, RoleFunction]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
