42770cdcbc2650a6e68ae003b8e2a61b20294604
Volha Khudaleyeva <olga.hudoleeva98@gmail.com>
2021-02-04 01:19:36 +0300

    Add services for offers to work with DB

--- src/service/offer.service.ts
+++ src/service/offer.service.ts
@@ -1,25 +1,56 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { OfferDTO } from 'src/dto/offer.dto';
+import { OfferMapper } from 'src/mapper/offer.mapper';
+import { Offer } from 'src/model/offer.model';
+import { Repository } from 'typeorm';
+import { OfferStatus } from 'src/constants';
 
 @Injectable()
 export class OfferService {
-  getOffers(): string {
-    return 'Offers list';
+  constructor(
+    @InjectRepository(Offer)
+    private readonly offerRepository: Repository<Offer>,
+    private readonly offerMapper: OfferMapper,
+  ) {}
+
+  getOffers(): Promise<OfferDTO[]> {
+    return this.offerRepository
+      .find()
+      .then((data) => data.map(this.offerMapper.toDto));
   }
 
-  getOfferByID(id: string): string {
-    return 'getOfferByID ' + id;
+  getOfferByID(id: string): Promise<OfferDTO> {
+    const offerPromise = this.offerRepository.findOne(id);
+    return offerPromise.then((offer) => this.offerMapper.toDto(offer));
   }
 
-  createOffer(offerDTO: OfferDTO): string {
-    return 'Было создано новое предложение.';
+  createOffer(offerDTO: OfferDTO): Promise<OfferDTO> {
+    return this.offerRepository
+      .save(this.offerMapper.toEntity(offerDTO))
+      .then((offer) => this.offerMapper.toDto(offer));
   }
 
-  deleteOffer(id: string): string {
-    return 'Предложение ' + id + ' было удалено.';
+  deleteOffer(id: string): Promise<OfferDTO> {
+    return this.offerRepository
+      .findOne(id)
+      .then((offer) => {
+        offer.status = OfferStatus.CLOSE;
+        return this.offerRepository.save(offer);
+      })
+      .then((offer) => this.offerMapper.toDto(offer));
   }
 
-  updateOffer(id: string): string {
-    return 'Предложение ' + id + ' было изменено.';
+  updateOffer(offerDTO: OfferDTO): Promise<OfferDTO> {
+    return this.offerRepository
+      .findOne(offerDTO.id)
+      .then((offer) => {
+        const updatedFields = this.offerMapper.toEntity(offerDTO);
+        for (const value in updatedFields) {
+          offer[value] = updatedFields[value];
+        }
+        return this.offerRepository.save(offer);
+      })
+      .then((offer) => this.offerMapper.toDto(offer));
   }
 }
